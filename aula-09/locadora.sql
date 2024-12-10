5d3ff38232f8a1f803a5956b986c9d95388e1f27
altemily <altemilyc@gmail.com>
2024-12-09 22:21:27 -0300

    Update: Concluir o banco de dados.

--- aula-09/locadora.sql
+++ aula-09/locadora.sql
@@ -1,31 +1,41 @@
 CREATE TABLE genres (
     id SERIAL PRIMARY KEY,
-    description VARCHAR(400) NOT NULL UNIQUE 
+    description VARCHAR(255) NOT NULL UNIQUE
 );
 
 CREATE TABLE movies (
     id SERIAL PRIMARY KEY,
-    name VARCHAR(400) NOT NULL,
-    id_genres INT NOT NULL, 
-    CONSTRAINT fk_genre FOREIGN KEY (id_genres) REFERENCES genres(id) ON DELETE CASCADE 
+    name VARCHAR(255) NOT NULL,
+    id_genres INT NOT NULL,
+    CONSTRAINT fk_genre FOREIGN KEY (id_genres) REFERENCES genres(id)
 );
 
--- CRIAR MAIS DOIS REGISTOS NA TABELA GENRES
+-- ESQUEMA DA FIGURA 1
+INSERT INTO genres (description) VALUES ('Animation');
 INSERT INTO genres (description) VALUES ('Horror');
 INSERT INTO genres (description) VALUES ('Action');
 INSERT INTO genres (description) VALUES ('Drama');
+INSERT INTO genres (description) VALUES ('Comedy');
+
+-- TABELA MOVIES
+INSERT INTO movies (name, id_genres) VALUES ('Batman', 3);
+INSERT INTO movies (name, id_genres) VALUES ('The Battle of the Dark River', 3);
+INSERT INTO movies (name, id_genres) VALUES ('White Duck', 1);
+INSERT INTO movies (name, id_genres) VALUES ('Breaking Barriers', 4);
+INSERT INTO movies (name, id_genres) VALUES ('The Two Hours', 2);
+
+-- CRIAR MAIS CINCO REGISTROS NA TEBELA MOVIES
+INSERT INTO movies (name, id_genres) VALUES ('Aliens', 2);
+INSERT INTO movies (name, id_genres) VALUES ('Beauty and the Beast', 1);
+INSERT INTO movies (name, id_genres) VALUES ('Avengers: Endgame', 3);
+INSERT INTO movies (name, id_genres) VALUES ('World War Z', 1);
+INSERT INTO movies (name, id_genres) VALUES ('Star Wars', 3);
+
+-- CRIAR MAIS DOIS REGISTROS NA TABELA GENRES
 INSERT INTO genres (description) VALUES ('Romance');
-INSERT INTO genres (description) VALUES ('Thriller');
+INSERT INTO genres (description) VALUES ('Adventure');
 
--- CRIAR MAIS CINCO REGISTROS
-INSERT INTO movies (name, id_genres) VALUES ('Aliens', 1);
-INSERT INTO movies (name, id_genres) VALUES ('Beauty and the Beast', 4);
-INSERT INTO movies (name, id_genres) VALUES ('Avengers: Endgame', 2);
-INSERT INTO movies (name, id_genres) VALUES ('World War Z', 5);
-INSERT INTO movies (name, id_genres) VALUES ('Star Wars', 2);
-
-
--- -- RETORNAR TODOS OS FILMES DE TERROR
+-- RETORNAR TODOS OS FILMES DE TERROR
 SELECT movies.name
 FROM movies
 JOIN genres ON movies.id_genres = genres.id
@@ -37,13 +47,13 @@ FROM movies
 JOIN genres ON movies.id_genres = genres.id
 WHERE genres.description IN ('Horror', 'Drama');
 
--- RETORNAR TODOS OS FILMES 
+-- RETORNAR TODOS OS FILMES
 SELECT * FROM movies;
 
--- RETORNAR TODOS OS GÊNEROS
+-- RETORNAR TODOS OS GENEROS
 SELECT * FROM genres;
 
--- RETORNAR TODOS OS FILMES COM A LETRA 'T'
+-- RETORNAR TODOS OS FILMES COM A LETRA "T"
 SELECT * FROM movies
 WHERE name LIKE 'T%';
 
@@ -52,7 +62,7 @@ UPDATE movies
 SET id_genres = (SELECT id FROM genres WHERE description = 'Drama')
 WHERE id_genres = (SELECT id FROM genres WHERE description = 'Action');
 
--- RETORNAR O NOME DE TODOS OS FILMES E O GÊNERO
+-- RETORNAR TODOS OS FILMES E GENEROS
 SELECT movies.name AS movie_name, genres.description AS genre_name
 FROM movies
 JOIN genres ON movies.id_genres = genres.id;
@@ -61,7 +71,3 @@ JOIN genres ON movies.id_genres = genres.id;
 DELETE FROM movies
 WHERE id_genres = (SELECT id FROM genres WHERE description = 'Action');
 
-
-
-
-\d movies
